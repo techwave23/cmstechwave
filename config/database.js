42b9532062a802010ba80716404fae48e73fa5e2
patricioreguera <reguerapatriciohernan@gmail.com>
2023-02-12 01:55:59 +0100

    fix: database host

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: "mysql",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "0.0.0.0"),
       port: env.int("DATABASE_PORT", 3306),
       database: env("DATABASE_NAME", "techwavedb"),
       user: env("DATABASE_USERNAME", "root"),
